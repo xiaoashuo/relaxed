8abdeb2e050bf31c57b941a3f6c246e3d401b3c6
Yakir <1412844668@qq.com>
2021-07-12 18:27:41 +0800

    :art:  batch insert auto fill

--- relaxed-extends/relaxed-extend-mybatis-plus/src/main/java/com/relaxed/extend/mybatis/plus/mapper/ExtendMapper.java
+++ relaxed-extends/relaxed-extend-mybatis-plus/src/main/java/com/relaxed/extend/mybatis/plus/mapper/ExtendMapper.java
@@ -1,17 +1,12 @@
 package com.relaxed.extend.mybatis.plus.mapper;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.core.toolkit.Constants;
-import com.baomidou.mybatisplus.extension.api.R;
+
 import com.baomidou.mybatisplus.extension.injector.methods.InsertBatchSomeColumn;
 import com.relaxed.extend.mybatis.plus.toolkit.ExtendConstants;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.Collection;
-
-import static com.baomidou.mybatisplus.core.toolkit.Constants.COLLECTION;
 
 /**
  * 所有的 Mapper接口 都需要继承当前接口 如果想自己定义其他的全局方法， 您的全局 BaseMapper 需要继承当前接口
@@ -26,7 +21,7 @@ public interface ExtendMapper<T> extends BaseMapper<T> {
 	 * @return int 改动行
 	 * @author lingting 2020-08-26 22:11
 	 */
-	int insertBatchSomeColumn(@Param("collection") Collection<T> list);
+	int insertBatchSomeColumn(@Param(ExtendConstants.COLLECTION) Collection<T> list);
 
 	/**
 	 * list insert
