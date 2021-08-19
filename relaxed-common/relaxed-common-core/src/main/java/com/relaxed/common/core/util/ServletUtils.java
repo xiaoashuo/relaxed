0b1ca6ccf451f24d448aa0bb3c2e40f3476e43bd
Yakir <1412844668@qq.com>
2021-08-19 11:07:52 +0800

    :sparkles: 扩充ServletUtils

--- relaxed-common/relaxed-common-core/src/main/java/com/relaxed/common/core/util/ServletUtils.java
+++ relaxed-common/relaxed-common-core/src/main/java/com/relaxed/common/core/util/ServletUtils.java
@@ -1,10 +1,15 @@
 package com.relaxed.common.core.util;
 
 import cn.hutool.core.convert.Convert;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.Assert;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.util.WebUtils;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -15,6 +20,7 @@ import java.io.IOException;
  *
  * @author ruoyi
  */
+@Slf4j
 public class ServletUtils {
 
 	/**
@@ -69,7 +75,10 @@ public class ServletUtils {
 	}
 
 	/**
-	 * 获取session
+	 * 获取Session
+	 * @author yakir
+	 * @date 2021/8/19 11:06
+	 * @return javax.servlet.http.HttpSession
 	 */
 	public static HttpSession getSession() {
 		return getRequest().getSession();
@@ -81,22 +90,93 @@ public class ServletUtils {
 	}
 
 	/**
-	 * 将字符串渲染到客户端
-	 * @param response 渲染对象
-	 * @param string 待渲染的字符串
-	 * @return null
+	 * 读取cookie
+	 * @param name cookie name
+	 * @return cookie value
 	 */
-	public static String renderString(HttpServletResponse response, String string) {
+	public String getCookieVal(String name) {
+		HttpServletRequest request = getRequest();
+		Assert.notNull(request, "request from RequestContextHolder is null");
+		return getCookieVal(request, name);
+	}
+
+	/**
+	 * 读取cookie
+	 * @param request HttpServletRequest
+	 * @param name cookie name
+	 * @return cookie value
+	 */
+	public String getCookieVal(HttpServletRequest request, String name) {
+		Cookie cookie = WebUtils.getCookie(request, name);
+		return cookie != null ? cookie.getValue() : null;
+	}
+
+	/**
+	 * 清除 某个指定的cookie
+	 * @param response HttpServletResponse
+	 * @param key cookie key
+	 */
+	public void removeCookie(HttpServletResponse response, String key) {
+		setCookie(response, key, null, 0);
+	}
+
+	/**
+	 * 设置cookie
+	 * @param response HttpServletResponse
+	 * @param name cookie name
+	 * @param value cookie value
+	 * @param maxAgeInSeconds maxage
+	 */
+	public void setCookie(HttpServletResponse response, String name, String value, int maxAgeInSeconds) {
+		Cookie cookie = new Cookie(name, value);
+		cookie.setPath("/");
+		cookie.setMaxAge(maxAgeInSeconds);
+		cookie.setHttpOnly(true);
+		response.addCookie(cookie);
+	}
+
+	/**
+	 * 将字符串渲染到客户端
+	 * @author yakir
+	 * @date 2021/8/19 11:03
+	 * @param response
+	 * @param result
+	 */
+	public static void renderString(HttpServletResponse response, String result) {
+		renderString(response, result, "application/json");
+	}
+
+	/**
+	 * 将字符串渲染到客户端
+	 * @author yakir
+	 * @date 2021/8/19 11:03
+	 * @param response
+	 * @param result
+	 * @param contentType
+	 */
+	public static void renderString(HttpServletResponse response, String result, String contentType) {
+		renderString(response, result, HttpStatus.OK.value(), contentType);
+	}
+
+	/**
+	 * 将字符串渲染到客户端
+	 * @author yakir
+	 * @date 2021/8/19 11:05
+	 * @param response
+	 * @param result
+	 * @param status
+	 * @param contentType
+	 */
+	public static void renderString(HttpServletResponse response, String result, Integer status, String contentType) {
 		try {
-			response.setStatus(200);
-			response.setContentType("application/json");
+			response.setStatus(status);
+			response.setContentType(contentType);
 			response.setCharacterEncoding("utf-8");
-			response.getWriter().print(string);
+			response.getWriter().print(result);
 		}
 		catch (IOException e) {
-			e.printStackTrace();
+			log.error(e.getMessage(), e);
 		}
-		return null;
 	}
 
 }
