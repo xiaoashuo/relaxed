85f30a338f62b95da47c84146d44af9fcfc6c5d7
Yakir <1412844668@qq.com>
2021-10-09 09:32:26 +0800

    :zap:  risk script test groovy for 需要加;号编译 分割语义 不然会被认为多编译  ide 运行没问题

--- relaxed-common/relaxed-common-risk/relaxed-common-risk-engine/src/test/java/com/relaxed/common/risk/engine/rule/ScriptTest.java
+++ relaxed-common/relaxed-common-risk/relaxed-common-risk-engine/src/test/java/com/relaxed/common/risk/engine/rule/ScriptTest.java
@@ -1,4 +1,5 @@
 package com.relaxed.common.risk.engine.rule;
+
 import com.relaxed.common.risk.engine.rules.EvaluateContext;
 import com.relaxed.common.risk.engine.rules.EvaluateReport;
 import com.relaxed.common.risk.engine.rules.script.groovy.GroovyResult;
@@ -22,27 +23,23 @@ import java.util.Map;
  */
 public class ScriptTest {
 
-    @Test
-    public void testScript(){
-        GroovyScriptHandler groovyScriptHandler = new GroovyScriptHandler();
+	@Test
+	public void testScript() {
+		GroovyScriptHandler groovyScriptHandler = new GroovyScriptHandler();
 
-         String ruleScript="class ActivationCheckScript {" +
-                 " public boolean check (def context,def data,def lists) { " +
-                 "def annexList=[ 1,2,7,9,12,20,21,24,25,35,36,37,38,42,45,54,69,74,76,77 ]; " +
-                 "def annexParams=context.eventJson.annexPackagePath.split (\",\");" +
-                 " for (param in annexParams){ " +
-                 "if (! annexList.contains (param as Integer)){" +
-                 " return true;" +
-                 " } }" +
-                 "return false;" +
-                 "}}";
-        EvaluateContext evaluateContext = new EvaluateContext();
-        Map<String, String> maps=new HashMap<>();
-        maps.put("annexPackagePath","1,2,3");
-        evaluateContext.setEventJson(maps);
-        EvaluateReport evaluateReport=new EvaluateReport();
-        GroovyResult groovyResult = groovyScriptHandler.invokeMethod(groovyScriptHandler.buildContext(ruleScript, "check", evaluateContext, evaluateReport, null));
-        System.out.println(groovyResult);
+		String ruleScript = "class ActivationCheckScript {" + " public boolean test (def context,def data,def lists) { "
+				+ "def annexList=[ 1,2,7,9,12,20,21,24,25,35,36,37,38,42,45,54,69,74,76,77 ]; "
+				+ "def annexParams=context.eventJson.annexPackagePath.split (\",\");" + " for (param in annexParams){ "
+				+ "if (! annexList.contains (param as Integer)){" + " return true;" + " } }; return false;" + "" + "}}";
+		EvaluateContext evaluateContext = new EvaluateContext();
+		Map<String, String> maps = new HashMap<>();
+		maps.put("annexPackagePath", "1,2,77");
+		evaluateContext.setEventJson(maps);
+		EvaluateReport evaluateReport = new EvaluateReport();
+		GroovyResult groovyResult = groovyScriptHandler.invokeMethod(
+				groovyScriptHandler.buildContext(ruleScript, "test", evaluateContext, evaluateReport, null));
+		System.out.println(groovyResult);
 
-    }
+	}
+
 }
