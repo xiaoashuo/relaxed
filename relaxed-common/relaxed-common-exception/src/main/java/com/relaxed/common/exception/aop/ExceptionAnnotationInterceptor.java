896e82d854ed1a7de6caec919a7ff279a7f1d260
1412844668@qq.com <12345698aA+>
2021-12-22 11:42:14 +0800

    :zap: 异常通知模块单次同步调用链，最外层捕获

--- relaxed-common/relaxed-common-exception/src/main/java/com/relaxed/common/exception/aop/ExceptionAnnotationInterceptor.java
+++ relaxed-common/relaxed-common-exception/src/main/java/com/relaxed/common/exception/aop/ExceptionAnnotationInterceptor.java
@@ -1,10 +1,14 @@
 package com.relaxed.common.exception.aop;
 
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
 import com.relaxed.common.exception.handler.GlobalExceptionHandler;
+import com.relaxed.common.exception.holder.ExceptionHolder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.util.StringUtils;
 
 /**
  * @author Yakir
@@ -21,6 +25,11 @@ public class ExceptionAnnotationInterceptor implements MethodInterceptor {
 
 	@Override
 	public Object invoke(MethodInvocation invocation) throws Throwable {
+		if (StringUtils.hasText(ExceptionHolder.getXID())) {
+			return invocation.proceed();
+		}
+		String xid = IdUtil.simpleUUID();
+		ExceptionHolder.bind(xid);
 		Object result;
 		try {
 			result = invocation.proceed();
@@ -28,6 +37,9 @@ public class ExceptionAnnotationInterceptor implements MethodInterceptor {
 		catch (Throwable throwable) {
 			globalExceptionHandler.handle(throwable);
 			throw throwable;
+		}
+		finally {
+			ExceptionHolder.unbind(xid);
 		}
 		return result;
 	}
