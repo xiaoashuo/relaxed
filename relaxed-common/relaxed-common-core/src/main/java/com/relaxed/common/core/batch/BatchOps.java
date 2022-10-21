4b03bd745afab2ff13c9f5a684f6733c7e70c37f
1412844668@qq.com <12345698aA+>
2022-10-21 13:47:08 +0800

    refactor(core):  增加批量ops默认实现

---
+++ relaxed-common/relaxed-common-core/src/main/java/com/relaxed/common/core/batch/BatchOps.java
@@ -0,0 +1,37 @@
+package com.relaxed.common.core.batch;
+
+import cn.hutool.core.thread.ThreadUtil;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author Yakir
+ * @Topic BatchOps
+ * @Description
+ * @date 2022/10/21 13:44
+ * @Version 1.0
+ */
+public class BatchOps  extends AbstractBatchOps {
+     /**
+     * 核心线程池大小
+     */
+     private static int coreSize = Runtime.getRuntime().availableProcessors();
+
+     /**
+     * 最大线程池大小
+     */
+     private static int maxSize = coreSize * 2 + 1;
+
+    /**
+     * 线程池
+     */
+    private static ThreadPoolExecutor executor = ThreadUtil.newExecutor(coreSize,
+     maxSize);
+
+
+    @Override
+    protected Executor executor() {
+        return executor;
+    }
+}
