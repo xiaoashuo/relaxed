e65ef5508b259fe1b202c2beb62de567fa2189e0
Yakir <1412844668@qq.com>
2025-06-05 10:54:09 +0800

    :feat 增加swagger描述 针对下载的响应

--- relaxed-starters/relaxed-spring-boot-starter-download/src/main/java/com/relaxed/starter/download/ResponseDownloadAutoConfiguration.java
+++ relaxed-starters/relaxed-spring-boot-starter-download/src/main/java/com/relaxed/starter/download/ResponseDownloadAutoConfiguration.java
@@ -1,17 +1,34 @@
 package com.relaxed.starter.download;
 
+import com.relaxed.starter.download.annotation.ResponseDownload;
 import com.relaxed.starter.download.aop.ResponseDownloadReturnValueHandler;
+import com.relaxed.starter.download.domain.DownloadModel;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-
+import com.relaxed.starter.download.annotation.ResponseDownload;
+import com.relaxed.starter.download.domain.DownloadModel;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
 import javax.annotation.PostConstruct;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,6 +59,49 @@ public class ResponseDownloadAutoConfiguration {
 		assert returnValueHandlers != null;
 		newHandlers.addAll(returnValueHandlers);
 		requestMappingHandlerAdapter.setReturnValueHandlers(newHandlers);
+	}
+
+	/**
+	 * 下载响应open api调整
+	 * @return doc响应自定义
+	 */
+	@Bean
+	@ConditionalOnClass(Operation.class)
+	public OperationCustomizer responseDownloadCustomizer() {
+		return (Operation operation, HandlerMethod handlerMethod) -> {
+			Method method = handlerMethod.getMethod();
+
+			// 检查方法是否有 @ResponseDownload 注解且返回类型为 DownloadModel
+			if (method.isAnnotationPresent(ResponseDownload.class)
+					&& DownloadModel.class.equals(method.getReturnType())) {
+
+				// 获取原 @Operation 注解
+				operation.setDescription(
+						"如果HTTP Status Code是200，则为application/octet-stream，返回文件流；否则为application/json，返回错误json信息");
+				ApiResponses responses = operation.getResponses();
+				if (responses == null) {
+					responses = new ApiResponses();
+					operation.setResponses(responses);
+				}
+				// 处理200成功响应
+
+				ApiResponse successResponse = responses.get("200");
+				if (successResponse == null) {
+					successResponse = new ApiResponse();
+					responses.addApiResponse("200", successResponse);
+				}
+				// 设置响应媒体类型为application/octet-stream
+				Content content = new Content();
+				MediaType mediaType = new MediaType();
+				mediaType.schema(
+						new Schema().type("string").format("binary").contentMediaType("application/octet-stream"));
+				content.addMediaType("application/octet-stream", mediaType);
+				successResponse.setContent(content);
+				successResponse.setDescription("操作成功,返回文件流");
+			}
+
+			return operation;
+		};
 	}
 
 }
